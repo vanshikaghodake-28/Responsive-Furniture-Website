f1c77ea09eeb76695e7a4662beddff784e0fab90
Vanshika Ghodake <105494665+vanshikaghodake-28@users.noreply.github.com>
2025-01-13 18:28:22 +0530

    Add files via upload
    Adding All coding files

---
+++ main.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener("click",function(){
+    navbar.classList.toggle('active');
+})
+// menu.onclick = () => {
+//    
+// }
+    // window.addEventListener("scroll",function(){
+    //     navbar.classList.remove('active');
+    // })
+    +// window.onscroll = () => {
+    
+// }
